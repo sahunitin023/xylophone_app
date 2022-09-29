998715f1014792ab0bc1570ca78bce15bb058b7a
sahunitin023 <b521039@iiit-bh.ac.in>
2022-09-29 15:56:48 +0530

    Minimised the line of codes using Functions & Completed the Project

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,41 @@ class XylophoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final player = AudioPlayer();
+
+    void playSound(int track) {
+      player.play(AssetSource('note$track.wav'));
+    }
+
+    Widget buildKey(Color color, int num) {
+      return Expanded(
+        child: ElevatedButton(
+          onPressed: () {
+            playSound(num);
+          },
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(color),
+          ),
+          child: const Text(' '),
+        ),
+      );
+    }
+
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              child: const Text(
-                'Click me',
-              ),
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.indigo, 6),
+              buildKey(Colors.purple, 7),
+            ],
           ),
         ),
       ),
